3ba43a8876dd55e90478ff479d98645149b5f52d
Tyronejames69 <mrbeastvalid21@gmail.com>
2025-07-23 20:48:53 -0500

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './Login';
+import './i18n';
 
+const App: React.FC = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  </BrowserRouter>
+);
+
+export default App;
